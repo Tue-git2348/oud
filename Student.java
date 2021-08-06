02751d33159c213edfa9b7d4c68260650a84603c
ffesit <88542303+ffesit@users.noreply.github.com>
2021-08-06 19:13:56 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SinhVien;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 84377
+ */
+public class Student implements Serializable{
+    public String id;
+    public String code;
+    public float gpa;
+    public String gpaLetter;
+    private static final long serialVersionUID = 20161107;
+
+    public Student(String id, String code, float gpa, String gpaLetter) {
+        this.id = id;
+        this.code = code;
+        this.gpa = gpa;
+        this.gpaLetter = gpaLetter;
+    }        
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getGpaLetter() {
+        return gpaLetter;
+    }
+
+    public void setGpaLetter(String gpaLetter) {
+        this.gpaLetter = gpaLetter;
+    }  
+    
+}
